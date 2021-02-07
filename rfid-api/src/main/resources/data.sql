a558cb7b49627cd36262df886e070a5f6bd2aa43
KHATAB YASSINE p2019960 <yassine.khatab@etu.univ-lyon1.fr>
2021-02-07 13:09:32 +0100

    Add CORS filter

--- rfid-api/src/main/resources/data.sql
+++ rfid-api/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into person (id,first_name,last_name,has_access,rfid)
-values('SJ920183N','John', 'DOE',FALSE,'SJ920183N');
+values('SJ920183N','Nassim', 'ELNOUALI',FALSE,'SJ920183N');
 insert into person (id,first_name,last_name,has_access,rfid)
 values('751ec40','Mohammed', 'EL GHAOUTH',TRUE,'751ec40');
 insert into person (id,first_name,last_name,has_access,rfid)
